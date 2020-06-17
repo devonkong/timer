f79ab05165c404d8994de2be8310591a2d06ec25
Devon Kong <devonkong@icloud.com>
2020-06-18 00:57:01 +1000

    Added basic GUI.

--- timer.py
+++ timer.py
@@ -1,19 +1,54 @@
-import time, datetime
-import threading
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Program initialised.')
+
+import kivy
+kivy.require('1.11.1')
+
+from kivy.app import App
+
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+import time
 
 
-def stopwatch():
+class HomeScreen(BoxLayout):
+    pass
+
+
+class TimerApp(App):
+    def build(self):
+        return HomeScreen()
     
-    input("Press ENTER to start.")
+    
+    
+# if __name__ == "__main__":
+#     TimerApp().run()
+
+
+start_time = 0
+end_time = 0
+
+def start_stopwatch():
+    global start_time
     start_time = time.time()
+    logging.debug(start_time)
 
-    input("Press ENTER to stop.")
+def stop_stopwatch():
+    global end_time
     end_time = time.time()
+    logging.debug(end_time)
 
-    elapsed_time = end_time - start_time
-    formatted_time = f"Elapsed time: {round(elapsed_time)}s."
-    
-    return formatted_time
+start_stopwatch()
+time.sleep(2)
+stop_stopwatch()
 
+elapsed_time = end_time - start_time
+logging.debug(elapsed_time)
+formatted_time = f"Elapsed time: {round(elapsed_time)}s."
 
-print(stopwatch())
+print(formatted_time)
+
+logging.debug('Program terminated.')
