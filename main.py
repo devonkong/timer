b444327605895eaee1feff6bb25a93ba6d3dba3e
Devon Kong <devonkong@icloud.com>
2020-06-18 23:27:03 +1000

    Added ability to continue or reset timer.

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from kivy.app import App
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+
+# Load kivy file
+from kivy.lang import Builder
+Builder.load_file("layout.kv")
 
 # Window settings
 from kivy.core.window import Window
@@ -14,52 +19,65 @@ Window.borderless = True
 from datetime import datetime, timedelta
 
 
-class HomeScreen(BoxLayout):
+class HomePage(Screen):
+    pass
     
+
+class TimerPage(Screen):
     start_time = 0
     end_time = 0
-    elapsed_time = 0
+    elapsed_time = timedelta(seconds=0)
     is_timing = False
     
     def timer_button(self, *args):
         """Starts or stops timer when timer button is pressed."""
-  
-        label = self.ids['timer_button']  # Refers to Label inside Button with id: timer_button
-            
         # If not currently timing, start timer
         if not self.is_timing:
             self.start_time = datetime.now()
             self.is_timing = True
             # TODO: Display stopwatch time in real-time
-            label.text = 'timer started'  # Update text attribute of Label
-        
+            self.ids['timer_button'].text = 'timer started'  # Update text attribute of Label
+            self.ids['reset_button'].opacity = 0  # Hide the reset button
         # If timer is running, stop timer and record time
         elif self.is_timing:
             self.end_time = datetime.now()
             self.is_timing = False
-            self.elapsed_time = self.end_time - self.start_time
-            label.text = self.format_time(self.elapsed_time)  # Display formatted elapsed time
+            self.elapsed_time += self.end_time - self.start_time
+            self.ids['timer_button'].text = self.format_time(self.elapsed_time)  # Display formatted elapsed time
+            self.ids['reset_button'].opacity = 1  # Show the reset button
            
         
     def format_time(self, timedelta_input):
         """Takes timedelta as input and returns formatted time as string value."""
-        
         total_secs = timedelta_input.total_seconds()
         hour = int(total_secs // 3600)
         mins = int((total_secs - 3600 * hour) // 60)
         secs = int((total_secs % 60) // 1)
-        
+        # Return MM:SS if under an hour, else return HH:MM:SS
         if hour == 0:
             return f"{mins:02}:{secs:02}"
         elif hour > 0:
             return f"{hour:02}:{mins:02}:{secs:02}"
-            
-    # TODO: Continue or reset timer
+                
+                
+    def reset_elapsed_time(self):
+        """If timer is not running when called, reset elapsed time and hide the reset button."""
+        if not self.is_timing:
+            self.is_timing = False
+            self.elapsed_time = timedelta(seconds=0)
+            self.ids['timer_button'].text = '00:00'  # Reset the display
+            self.ids['reset_button'].opacity = 0  # Hide the reset button
+
+    
+# Enable navigation between pages
+navigator = ScreenManager(transition=NoTransition())
+navigator.add_widget(TimerPage(name='timer'))
+navigator.add_widget(HomePage(name='home'))  # TODO: Put at top of navigator list
 
             
 class TimerApp(App):
     def build(self):
-        return HomeScreen()
+        return navigator
     
 if __name__ == "__main__":
     TimerApp().run()
