eac63547bc7048cae8f1934744e19cbe32a69e41
Devon Kong <devonkong@icloud.com>
2020-06-18 13:45:15 +1000

    Mapped basic stopwatch functionality to GUI.

--- timer.py
+++ timer.py
@@ -1,56 +1,45 @@
-import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.debug('Program initialised.')
-
 import kivy
 kivy.require('1.11.1')
 
 from kivy.app import App
 
 from kivy.uix.button import Button
-from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 
 import time
 
 
 class HomeScreen(BoxLayout):
-    pass
-
+    
+    start_time = 0
+    end_time = 0
+    elapsed_time = 0
+    is_timing = False
+    
+    def timer_button(self, *args):
+        # Runs when the timer button is pressed
+        
+        label = self.ids['timer_button']  # Refers to Label inside Button with id: timer_button
+        
+        # If not currently timing, start timer
+        if not self.is_timing:
+            self.start_time = time.time()
+            self.is_timing = True
+            label.text = 'timer started'  # Update text attribute of Label
+        
+        # If timer is running, stop timer and record time
+        elif self.is_timing:
+            self.end_time = time.time()
+            self.is_timing = False
+            self.elapsed_time = self.end_time - self.start_time
+            label.text = f"{round(self.elapsed_time)}s"  # Display elapsed time
+            
 
 class TimerApp(App):
     def build(self):
         return HomeScreen()
     
-    
-    
 if __name__ == "__main__":
     TimerApp().run()
 
 
-start_time = 0
-end_time = 0
-
-def start_stopwatch():
-    global start_time
-    start_time = time.time()
-    logging.debug(start_time)
-    return start_time
-
-def stop_stopwatch():
-    global end_time
-    end_time = time.time()
-    logging.debug(end_time)
-    return end_time
-
-# start_stopwatch()
-# time.sleep(2)
-# stop_stopwatch()
-
-# elapsed_time = end_time - start_time
-# logging.debug(elapsed_time)
-# formatted_time = f"Elapsed time: {round(elapsed_time)}s."
-
-# print(formatted_time)
-
-logging.debug('Program terminated.')
