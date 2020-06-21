53a085b4740fe3632f15792174957e26e46ad137
Devon Kong <devonkong@icloud.com>
2020-06-21 17:25:52 +1000

    Timer is now able to go beyond 99 hours.

--- main.py
+++ main.py
@@ -1,40 +1,36 @@
+from datetime import datetime, timedelta
+from kivy.core.window import Window
+from kivy.config import Config
+import kivy.utils as utils
+from kivy.lang import Builder
+from kivy.clock import Clock
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
 import kivy
 kivy.require('1.11.1')
 
-from kivy.app import App
-
-# Kivy UI elements
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
-
-from kivy.clock import Clock
 
 # Load Kivy file
-from kivy.lang import Builder
 Builder.load_file("style.kv")
 
 # Set colour palette
-import kivy.utils as utils
 bg_col = utils.get_color_from_hex('#1E1E1E')
 text_col_main = utils.get_color_from_hex('#DDDDDD')
 text_col_secondary = utils.get_color_from_hex('#555555')
 
 # App settings
-from kivy.config import Config
 Config.set('kivy', 'exit_on_escape', 'True')
 Config.set('graphics', 'resizable', 'False')
 
 # Window settings
-from kivy.core.window import Window
 Window.size = (400, 500)
 Window.top = 0
 Window.left = 0
 Window.borderless = False
 Window.fullscreen = False
 Window.clearcolor = bg_col
-
-from datetime import datetime, timedelta
 
 
 class HomePage(Screen):
@@ -61,24 +57,26 @@ class TimerPage(Screen):
             self.end_time = datetime.now()
             self.elapsed_time += self.end_time - self.start_time
             self.ids['start_stop_button'].color = text_col_secondary
-            self.ids['start_stop_button'].text = self.format_time(self.elapsed_time)  # Display formatted elapsed time
+            self.ids['start_stop_button'].text = self.format_time(
+                self.elapsed_time)  # Display formatted elapsed time
             self.ids['reset_button'].opacity = 1  # Show the reset button
 
     def format_time(self, timedelta_input):
-            """Takes timedelta as input and returns formatted time as string value."""
-            total_secs = timedelta_input.total_seconds()
-            hour = int(total_secs // 3600)
-            mins = int((total_secs - 3600 * hour) // 60)
-            secs = int((total_secs % 60) // 1)
-            # Return MM:SS if under an hour, else return HH:MM:SS
-            if hour == 0:
-                return f"{mins:02}:{secs:02}"
-            elif hour > 0:
-                return f"{hour:02}:{mins:02}:{secs:02}"
+        """Takes timedelta as input and returns formatted time as string value."""
+        total_secs = timedelta_input.total_seconds()
+        hours = int(total_secs // 3600)
+        mins = int((total_secs - 3600 * hours) // 60)
+        secs = int((total_secs % 60) // 1)
+        # Return MM:SS if under an hours, else return HH:MM:SS
+        if hours == 0:
+            return f"{mins:02}:{secs:02}"
+        elif hours > 0:
+            return f"{hours}:{mins:02}:{secs:02}"
 
     def update_time(self, *args):
         """Update timer display."""
-        self.ids['start_stop_button'].text = self.format_time(datetime.now() - self.start_time + self.elapsed_time)
+        self.ids['start_stop_button'].text = self.format_time(
+            datetime.now() - self.start_time + self.elapsed_time)
 
     def reset_elapsed_time(self):
         """If timer is not running and has not already been reset when called, reset elapsed time and hide the reset button."""
